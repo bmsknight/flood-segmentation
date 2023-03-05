8a01c1652e86fd24fc8d8f7f1b6c6bbf69d3c2fa
udanish50 <112751117+udanish50@users.noreply.github.com>
2023-03-05 14:47:47 -0500

    Update EfficientNeuralNetwork.py

--- scripts/EfficientNeuralNetwork.py
+++ scripts/EfficientNeuralNetwork.py
@@ -1,8 +1,12 @@
-import os 
-import tensorflow as tf
+import os
+import random
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Concatenate, BatchNormalization, Dropout
 from tensorflow.keras.models import Model
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from plot_keras_history import plot_history
 import voxelmorph as vxm
 tf.compat.v1.disable_eager_execution()
 tf.compat.v1.experimental.output_all_intermediates(True)
@@ -60,8 +64,26 @@ model_dir = '/content/drive/MyDrive/data'
 save_filename = os.path.join(model_dir, '{epoch:04d}.h5')
 save_callback = tf.keras.callbacks.ModelCheckpoint(save_filename, period=50)
 epochs = 201
-loss = 'binary_crossentropy'
+loss0 = 'binary_crossentropy'
+loss1 = 'dice'
 
-model.compile(optimizer='adam', loss=loss, metrics=['accuracy'])
-model.fit(Xtrain, ytrain, validation_split=0.2, batch_size=5, verbose=True, epochs=epochs, callbacks=callbacks)
+model.compile(optimizer='adam', loss=loss1, metrics=['accuracy'])
+history=model.fit(Xtrain, ytrain, validation_split=0.2, batch_size=5, verbose=True, epochs=epochs, callbacks=callbacks)
 model.save(save_filename.format(epoch=epochs))
+plot_history(history, path="singleton", single_graphs=True)
+plt.close()
+
+ind = random.randint(0, len(X))
+img = X[ind]
+predMask = model.predict(np.expand_dims(img, axis=0), verbose=0)
+source = ColumnDataSource(data={
+    'image': [X[ind]],
+    'mask': [np.squeeze(Y[ind])],
+    'pred_mask': [np.squeeze(predMask)]
+})
+fig = figure(plot_width=900, plot_height=300, title='Image, Mask, and Predicted Mask')
+fig.title.align = 'center'
+fig.image(image='image', x=0, y=0, dw=300, dh=300, source=source)
+fig.image(image='mask', x=300, y=0, dw=300, dh=300, source=source)
+fig.image(image='pred_mask', x=600, y=0, dw=300, dh=300, source=source)
+show(fig)
