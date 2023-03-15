6acd0954fdea0309c2dc7502f78eeaeaebd59ce4
Tehara-Fonseka <thanuritehara@gmail.com>
2023-03-15 05:20:05 -0400

    Add START_EPOCH constant

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 TRAIN_DATA_DIR = "C:\\Users\\tehar\\OneDrive\\Documents\\Canada\\M.E.Sc\\ML_9309\\Project\\flood-segmentation\\data\\train"
 TEST_DATA_DIR = "C:\\Users\\tehar\\OneDrive\\Documents\\Canada\\M.E.Sc\\ML_9309\\Project\\flood-segmentation\\data\\test"
+RANDOM_SEED = 0
 IMG_H = 256
 IMG_W = 256
 IN_CHANNELS = 3
@@ -7,5 +8,5 @@ OUT_CHANNELS = 1
 BATCH_SIZE = 8
 SHUFFLE = True
 LEARNING_RATE = 1e-5
-EPOCHS = 500
-RANDOM_SEED = 0
+EPOCHS = 50
+START_EPOCH = 0
